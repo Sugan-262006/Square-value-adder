60072fe83edc3bc6119b1a8d530fde169e490240
SUGAN.R <148607729+Sugan-262006@users.noreply.github.com>
2023-12-30 07:54:26 +0530

    Add files via upload

---
+++ powerbill.py
@@ -0,0 +1,5 @@
+a=int(input("Enter service number:  "))
+b=int(input("Enter previous month meter value:   "))
+c=int(input("Enter current month meter value:    "))
+d=c-b
+print ("your bill amount is:",d*3)
